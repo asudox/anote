331ea9ae058d81defb8f61b9d2a65cb50d638a05
Asudox <developer@asudox.dev>
2024-04-04 22:54:51 +0200

    made the watched animes filtering more efficient and faster using a HashSet

--- src/utils/filter_watched_animes.rs
+++ src/utils/filter_watched_animes.rs
@@ -1,15 +1,13 @@
 use crate::structs::{Anime, DbGenreComboWithAnimeScores};
+use std::collections::HashSet;
 
 /// Filters out the users already watched animes from the given user_db_genre_combo
 pub async fn filter_watched_animes(
     user_animes: &[Anime],
     user_db_genre_combo: &mut DbGenreComboWithAnimeScores,
 ) {
-    user_db_genre_combo.anime_scores.retain(|(db_anime, _)| {
-        !user_animes
-            .iter()
-            .map(|anime| anime.id)
-            .collect::<Vec<i32>>()
-            .contains(&db_anime.id)
-    })
+    let watched_anime_ids: HashSet<i32> = user_animes.iter().map(|anime| anime.id).collect();
+    user_db_genre_combo
+        .anime_scores
+        .retain(|(db_anime, _)| !watched_anime_ids.contains(&db_anime.id));
 }
